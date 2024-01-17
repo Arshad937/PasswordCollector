4b1e569b123bd7d255bdae44aff8eb94bb843153
arshad <ak849496@gmail.com>
2024-01-17 16:08:31 +0500

    Initial Commit

---
+++ PasswordCollector/PasswordCollectorApp.swift
@@ -0,0 +1,17 @@
+//
+//  PasswordCollectorApp.swift
+//  PasswordCollector
+//
+//  Created by Arshad Mustafa on 17/01/2024.
+//
+
+import SwiftUI
+
+@main
+struct PasswordCollectorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
